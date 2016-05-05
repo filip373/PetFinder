64d0588df65e83a0f80ca97bb3e746f807b75a36
Sebastian Potasiak <sebastian.potasiak@gmail.com>
2016-05-05 09:17:30 +0200

    Zmieniona nazwa kontrolera REST użytkownikow ze względu na możliwe konflikty

--- src/main/java/com/petfinder/rest/controller/UserRestController.java
+++ src/main/java/com/petfinder/rest/controller/UserRestController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
-public class UserController {
+public class UserRestController {
 
     @Autowired
     UserService userService;
