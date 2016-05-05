fd0592f615d1df58ba70f40fc213b383f7fe3618
Sebastian Potasiak <sebastian.potasiak@gmail.com>
2016-05-05 09:16:29 +0200

    Dodana adnotacja RequestMapping do kontrolera rejestracji uzytkownika

--- src/main/java/com/petfinder/rest/controller/UserController.java
+++ src/main/java/com/petfinder/rest/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,6 +21,7 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @RequestMapping(value = "register", method = RequestMethod.POST)
     public ResponseEntity<Status> register(
             @RequestBody UserRegistrationForm data) {
         try {
