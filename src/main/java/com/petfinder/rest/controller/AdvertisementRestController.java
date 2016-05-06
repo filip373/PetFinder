683668711798cb2d59eb5227f3332467b586ce6f
Sebastian Potasiak <sebastian.potasiak@gmail.com>
2016-05-06 15:58:06 +0200

    Dodana usługa ogłoszeń oraz kontroler REST

---
+++ src/main/java/com/petfinder/rest/controller/AdvertisementRestController.java
@@ -0,0 +1,54 @@
+package com.petfinder.rest.controller;
+
+import com.petfinder.domain.Advertisement;
+import com.petfinder.rest.domain.RestResponse;
+import com.petfinder.service.AdvertisementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/advertisement")
+public class AdvertisementRestController {
+
+    @Autowired
+    AdvertisementService advertisementService;
+
+    @RequestMapping(value = "latest", method = RequestMethod.GET)
+    public ResponseEntity<?> getLatest() {
+        ResponseEntity<List<Advertisement>> response = null;
+        try {
+            response = new ResponseEntity<>(
+                    advertisementService.getLatestAdvertisements(1),
+                    HttpStatus.OK
+            );
+        } catch (Exception e) {
+            return new ResponseEntity<RestResponse>(
+                    new RestResponse(e.getMessage(), 1, "unexpected error"),
+                    HttpStatus.BAD_REQUEST
+            );
+        }
+        return response;
+    }
+
+    @RequestMapping(value = "latest/{page}", method = RequestMethod.GET)
+    public ResponseEntity<?> getLatest(
+            @PathVariable int page) {
+        ResponseEntity<List<Advertisement>> response = null;
+        try {
+             response = new ResponseEntity<>(
+                    advertisementService.getLatestAdvertisements(page),
+                    HttpStatus.OK
+            );
+        } catch (Exception e) {
+            return new ResponseEntity<RestResponse>(
+                    new RestResponse(e.getMessage(), 1, "unexpected error"),
+                    HttpStatus.BAD_REQUEST
+            );
+        }
+        return response;
+    }
+}
