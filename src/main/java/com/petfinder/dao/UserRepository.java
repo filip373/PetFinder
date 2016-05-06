a6933efeb591370246b92f5f7d2a78530d1f109d
Sebastian Potasiak <sebastian.potasiak@gmail.com>
2016-05-06 10:58:33 +0200

    Poprawki w UserRepository i UserService

--- src/main/java/com/petfinder/dao/UserRepository.java
+++ src/main/java/com/petfinder/dao/UserRepository.java
@@ -1,18 +1,28 @@
 package com.petfinder.dao;
 
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import com.petfinder.domain.User;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
 
 public interface UserRepository extends CrudRepository<User, Long>{
-	
-	//@Modifying
-	//@Query("SELECT u FROM User u where u.login=:login")
-	//@Param("login") 
-	User findOneByLogin(String login);
-	
-	User findOneByEmail(String email);
+
+    @Transactional
+	@Query("SELECT u FROM User u where u.login=:login")
+	User findOneByLogin(@Param("login") String login);
+
+    @Transactional
+    @Query("SELECT u FROM User u where u.email=:email")
+	User findOneByEmail(@Param("email") String email);
+
+    @Transactional
+	@Query("SELECT CASE WHEN COUNT(u) > 0 THEN 'true' ELSE 'false' END FROM User u WHERE u.login=:login")
+	Boolean existsByLogin(@Param("login") String login);
+
+    @Transactional
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN 'true' ELSE 'false' END FROM User u WHERE u.email=:email")
+    Boolean existsByEmail(@Param("email") String email);
 }
