945784afa255b3fbe1c887770ee821afa84a48e4
Agnieszka Kubiś <207570@edu.p.lodz.pl>
2016-05-10 00:04:24 +0200

    Dodawanie nowych ogłoszeń

--- src/main/java/com/petfinder/service/AdvertisementService.java
+++ src/main/java/com/petfinder/service/AdvertisementService.java
@@ -1,9 +1,11 @@
 package com.petfinder.service;
 
 import com.petfinder.dao.AdvertisementRepository;
+import com.petfinder.dao.AttachmentRepository;
 import com.petfinder.dao.LocationRepository;
 import com.petfinder.dao.PetRepository;
 import com.petfinder.dao.PetCategoryRepository;
+import com.petfinder.dao.TagRepository;
 import com.petfinder.domain.Advertisement;
 import com.petfinder.domain.Attachment;
 import com.petfinder.domain.Location;
@@ -30,6 +32,10 @@ public class AdvertisementService {
     PetCategoryRepository petCategoryRepository;
     @Autowired
     PetRepository petRepository;
+    @Autowired
+    TagRepository tagRepository;
+    @Autowired
+    AttachmentRepository attachmentRepository;
 
     @Transactional
     public List<Advertisement> getLatestAdvertisements(int page, int perPage) {
@@ -63,18 +69,36 @@ public class AdvertisementService {
     }
 
     @Transactional
-    public void newAdvertisement(String title, String content, String petName, Integer age, String race, String categoryName, String voivodership, String commune, String place) {
-        //TODO user, tags, attachments
+    public void newAdvertisement(String title, String content, String petName, Integer age, String race, String categoryName, String voivodership, String commune, String place, List<Tag> tags, List<Attachment> attachments) {
+        //TODO user, który jest teraz zalogowany
         User user = null;
-        PetCategory petCategory = new PetCategory(categoryName);
-        petCategoryRepository.save(petCategory);
+        PetCategory petCategory = null;
+        if (petCategoryRepository.findByName(categoryName).isEmpty()) {
+            petCategory = new PetCategory(categoryName);
+            petCategoryRepository.save(petCategory);
+        } else {
+            petCategory = petCategoryRepository.findByName(categoryName).get(0);
+        }
         Pet pet = new Pet(petName, race, age, user, petCategory);
         petRepository.save(pet);
-        Location location= new Location(voivodership, place, commune);
+        Location location = new Location(voivodership, place, commune);
         locationRepository.save(location);
-       List<Tag> tags=null;
-       List<Attachment> attachments=null;
-        Advertisement advertisement= new Advertisement(title, content, user, pet, location, tags, attachments);
+        for (int i = 0; i < tags.size(); i++) {
+            if (tagRepository.findOneByName(tags.get(i).getName()) == null) {
+                tagRepository.save(tags.get(i));
+            } else {
+                tags.set(i, tagRepository.findOneByName(tags.get(i).getName()));
+            }
+        }
+        for (Attachment attachment : attachments) {
+            attachmentRepository.save(attachment);
+        }
+
+        Advertisement advertisement = new Advertisement(title, content, user, pet, location, tags, attachments);
+        for (Attachment attachment : attachments) {
+            attachment.setAdvertisement(advertisement);
+        }
+        advertisement.setAttachments(attachments);
         advertisementRepository.save(advertisement);
     }
 }
