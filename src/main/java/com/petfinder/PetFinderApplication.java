a0fa38c821d48529867c8bf70da30afb9418304d
Sebastian Potasiak <sebastian.potasiak@gmail.com>
2016-05-29 12:18:58 +0200

    Fixed styles, added example random data

--- src/main/java/com/petfinder/PetFinderApplication.java
+++ src/main/java/com/petfinder/PetFinderApplication.java
@@ -1,6 +1,11 @@
 package com.petfinder;
 
+import com.lyncode.jtwig.mvc.JtwigViewResolver;
+import com.petfinder.domain.Tag;
+import com.petfinder.service.AdvertisementService;
+import com.petfinder.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,16 +15,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 
-import com.lyncode.jtwig.mvc.JtwigViewResolver;
-import com.petfinder.service.AdvertisementService;
-import com.petfinder.service.UserService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @SpringBootApplication
 @PropertySource(value = { "classpath:application.properties" })
 @EnableJpaRepositories
 @EnableAutoConfiguration
 @EnableTransactionManagement
-public class PetFinderApplication {
+public class PetFinderApplication implements CommandLineRunner {
 
     @Autowired
     AdvertisementService advertisementService = new AdvertisementService();
@@ -30,6 +35,42 @@ public class PetFinderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PetFinderApplication.class, args);
 	}
+
+    @Override
+    public void run(String... strings) throws Exception {
+        Random rand = new Random();
+        Tag atags[] = {
+                new Tag("tag1"), new Tag("tag2"), new Tag("tag3"), new Tag("tag4"),
+                new Tag("tag5"), new Tag("tag6"), new Tag("tag7"), new Tag("tag8"),
+                new Tag("tag9"), new Tag("tag10"), new Tag("tag11"), new Tag("tag12")
+        };
+        String atitles[] = {
+                "Zgubiono ", "Zaginął ", "POMOCY GDZIE JEST MÓJ ", "Znaleziono ",
+                "Znaleziony ", "Odzyskano ", "Sprzedam opla i "
+        };
+        String apets[] = {
+                "jeż", "pies", "kot", "żyrafa", "słoń", "leniwiec", "orangutan"
+        };
+        String anames[] = {
+                "Henryk", "Andrzej", "Manuela", "Bogdan", "Wiesiek", "Bożydar"
+        };
+        List<Tag> tags = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            tags.clear();
+            int tc = rand.nextInt(5);
+            for (int j = 0; j < tc; j++) {
+                tags.add(atags[rand.nextInt(atags.length)]);
+            }
+            String pet = apets[rand.nextInt(apets.length)];
+            String title = String.format("%s%s", atitles[rand.nextInt(atitles.length)],
+                    pet);
+            advertisementService.newAdvertisement(
+                    title, "Test", anames[rand.nextInt(anames.length)],
+                    rand.nextInt(20), pet, "tojestbezsensu",
+                    "Łódzkie", "Widzew", "Łódź", tags, new ArrayList<>()
+            );
+        }
+    }
 
     @Bean
     public ViewResolver viewResolver() {
